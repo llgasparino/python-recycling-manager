f6eb8f7aa244f98594867c501651f82cf7e9e930
llgasparino <luizgasparino48@gmail.com>
2025-08-05 22:51:28 -0300

    adicionado novas dependencias

--- recycling-manager/__init__.py
+++ recycling-manager/__init__.py
@@ -3,9 +3,11 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.errorhandler(404)
 def page_not_found(error):
