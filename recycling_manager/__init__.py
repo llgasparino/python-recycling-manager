e05c94befae6210971c68b1a793454b1e5eed9fd
llgasparino <luizgasparino48@gmail.com>
2025-09-07 19:45:20 -0300

    Adicionado testes unitarios

--- recycling_manager/__init__.py
+++ recycling_manager/__init__.py
@@ -1,14 +1,11 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
-
 @app.errorhandler(404)
-def page_not_found(error):
-    return render_template('page_not_found.html'), 404
+def page_not_found(e):
+    return render_template("page_not_found.html"), 404
--- tests/test_hello_pytest.py
+++ tests/test_hello_pytest.py
@@ -1,3 +1,4 @@
 
 def test_hello():
     assert 1 + 1 == 2
+    
