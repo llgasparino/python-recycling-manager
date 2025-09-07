23eb035f3609705d5d8edbba0051bdf5097703fc
llgasparino <luizgasparino48@gmail.com>
2025-09-07 19:52:51 -0300

    update:Dockerfile

--- recycling_manager/__init__.py
+++ recycling_manager/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.errorhandler(404)
 def page_not_found(e):
--- tests/test_hello_pytest.py
+++ tests/test_hello_pytest.py
@@ -1,4 +1,2 @@
-
 def test_hello():
     assert 1 + 1 == 2
-    
